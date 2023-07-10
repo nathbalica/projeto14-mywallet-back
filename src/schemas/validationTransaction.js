c3aa23c99ea9a56a5f94dc779536f72712cdf892
nathbalica <nathbalica@gmail.com>
2023-07-10 11:36:47 -0300

    update modifications transaction

--- src/schemas/validationTransaction.js
+++ src/schemas/validationTransaction.js
@@ -10,22 +10,10 @@ export const transactionSchema = joi.object({
         'any.required': 'O campo "description" é obrigatório.',
         'string.empty': 'O campo "description" não pode estar vazio.',
       }),
-    type: joi.string().valid('entrada', 'saida').required().messages({
+    type: joi.string().required().valid('profit', 'expense').messages({
         'any.required': 'O campo "type" é obrigatório.',
         'string.empty': 'O campo "type" não pode estar vazio.',
-        'any.only': 'O campo "type" deve ser "entrada" ou "saida".',
+        'any.only': 'O campo "type" deve ser "profit" ou "expense".',
     }),
 })
 
-export const transactionUpdateSchema = joi.object({
-    value: joi.number().positive().required().messages({
-        'any.required': 'O campo "email" é obrigatório.',
-        'string.empty': 'O campo "email" não pode estar vazio.',
-        'string.email': 'O campo "email" deve ter um formato válido.',
-    }),
-    description: joi.string().required().messages({
-        'any.required': 'O campo "description" é obrigatório.',
-        'string.empty': 'O campo "description" não pode estar vazio.',
-      })
-
-})
